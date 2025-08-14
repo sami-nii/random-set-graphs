9d7751a2d1ded1a2de8c2b9527935f6b77b5cb38
matteotolloso <matteo.tolloso@phd.unipi.it>
2025-08-14 22:43:05 +0000

    plots

--- run_all.sh
+++ run_all.sh
@@ -1,26 +1,13 @@
 
 
-# python main.py --dataset chameleon --model vanilla --count 100    ;
-# python main.py --dataset chameleon --model ensemble  ;
-
-# python main.py --dataset patents --model vanilla --count 100    ;
-# python main.py --dataset patents --model ensemble  ;
-
-# python main.py --dataset arxiv --model vanilla --count 100    ;
-# python main.py --dataset arxiv --model ensemble  ;  
-
+python main.py --dataset chameleon --model vanilla --count 100    ;
+python main.py --dataset patents --model vanilla --count 100    ;
+python main.py --dataset arxiv --model vanilla --count 100    ;
 python main.py --dataset reddit2 --model vanilla --count 100    ;
-python main.py --dataset reddit2 --model ensemble  ;
-
 python main.py --dataset coauthor --model vanilla --count 100    ;
-python main.py --dataset coauthor --model ensemble  ;
+python main.py --dataset squirrel --model vanilla --count 100    ;
 
-# python main.py --dataset squirrel --model vanilla --count 100    ;
-# python main.py --dataset squirrel --model ensemble  ;
-
-
-
-# python main.py --dataset chameleon --model credal --count 30    ;
+python main.py --dataset chameleon --model credal --count 30    ;
 python main.py --dataset patents --model credal --count 30   ;
 python main.py --dataset arxiv --model credal --count 30    ;
 python main.py --dataset reddit2 --model credal --count 30    ;
@@ -33,3 +20,10 @@ python main.py --dataset arxiv --model credal_LJ --count 30    ;
 python main.py --dataset reddit2 --model credal_LJ --count 30    ;
 python main.py --dataset coauthor --model credal_LJ --count 30    ;
 python main.py --dataset squirrel --model credal_LJ --count 30    ;
+
+python main.py --dataset squirrel --model ensemble  ;
+python main.py --dataset arxiv --model ensemble  ;  
+python main.py --dataset coauthor --model ensemble  ;
+python main.py --dataset reddit2 --model ensemble  ;
+python main.py --dataset patents --model ensemble  ;
+python main.py --dataset chameleon --model ensemble  ;
